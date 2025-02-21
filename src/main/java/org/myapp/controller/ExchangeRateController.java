1c669588ed11a8b7f7f669552ba22d0ab002b29b
aveasura <aveasura@gmail.com>
2025-02-21 20:47:09 +0300

    [feat] exchangeRate doGet
    Теперь отправляет json ответ

--- src/main/java/org/myapp/controller/ExchangeRateController.java
+++ src/main/java/org/myapp/controller/ExchangeRateController.java
@@ -1,5 +1,6 @@
 package org.myapp.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,12 +11,11 @@ import org.myapp.service.CurrenciesService;
 import org.myapp.service.ExchangeRateService;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.List;
 
 @WebServlet("/exchangeRates")
 public class ExchangeRateController extends HttpServlet {
-
+    private ObjectMapper objectMapper = new ObjectMapper();
     private ExchangeRateService exchangeRateService;
     private CurrenciesService currencyService;
 
@@ -25,30 +25,20 @@ public class ExchangeRateController extends HttpServlet {
         currencyService = (CurrenciesService) getServletContext().getAttribute("currencyService");
     }
 
+    // TODO dto
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<ExchangeRate> exchangeRateList = exchangeRateService.getExchangeRates();
-
-        req.setAttribute("ratesList", exchangeRateList);
-        req.getRequestDispatcher("/WEB-INF/views/exchangeList.jsp").forward(req, resp);
+        sendJsonResponse(resp, exchangeRateList, HttpServletResponse.SC_OK);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String baseCurrId = req.getParameter("from");
-        String targetCurrId = req.getParameter("to");
-        String rate = req.getParameter("rate");
+    private void sendJsonResponse(HttpServletResponse resp, Object responseObject, int status) throws IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setStatus(status);
 
-//        CurrencyDto from = currencyService.getCurrency(baseCurrId);
-//        CurrencyDto to = currencyService.getCurrency(targetCurrId);
-
-        try {
-            BigDecimal exchangeRate = new BigDecimal(rate);
-//            service.addExchangeRate(from, to, exchangeRate);
-            resp.sendRedirect(req.getContextPath() + "/exchangeRates");
-        } catch (NumberFormatException e) {
-            req.setAttribute("error", "Ошибка: курс валют должен быть числом.");
-            req.getRequestDispatcher("/exchangeRatesForm.jsp").forward(req, resp);
-        }
+        objectMapper.writeValue(resp.getWriter(), responseObject);
     }
+
+    // TODO patch
 }
