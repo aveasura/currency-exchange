f2b7aa24a3605ed8e2a7598a114cc2c4cbab153c
aveasura <aveasura@gmail.com>
2026-03-30 17:35:16 +0300

    feat: add context attributes

---
+++ src/main/java/org/exchanger/config/ContextAttributes.java
@@ -0,0 +1,10 @@
+package org.exchanger.config;
+
+public final class ContextAttributes {
+    public static final String CURRENCY_REPOSITORY = "currencyRepository";
+    public static final String EXCHANGE_RATE_REPOSITORY = "exchangeRateRepository";
+    public static final String CURRENCY_SERVICE = "currencyService";
+    public static final String EXCHANGE_RATE_SERVICE = "exchangeRateService";
+    public static final String EXCHANGE_SERVICE = "exchangeService";
+    public static final String OBJECT_MAPPER = "objectMapper";
+}
