b772840b01e7898d43d5c15aa9fb8542d685c568
aveasura <aveasura@gmail.com>
2026-03-30 18:49:56 +0300

    ref: improve currency parser

--- src/main/java/org/exchanger/servlet/parser/CurrencyRequestParser.java
+++ src/main/java/org/exchanger/servlet/parser/CurrencyRequestParser.java
@@ -2,8 +2,12 @@ package org.exchanger.servlet.parser;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.exchanger.dto.request.CurrencyRequest;
+import org.exchanger.exception.BadRequestException;
 
 public class CurrencyRequestParser extends AbstractRequestParser<CurrencyRequest> {
+
+    private static final String INVALID_CODE_MESSAGE =
+            "Currency code should contain exactly 3 latin letters. Example: EUR";
 
     private static final String NAME_PARAM = "name";
     private static final String CODE_PARAM = "code";
@@ -17,6 +21,15 @@ public class CurrencyRequestParser extends AbstractRequestParser<CurrencyRequest
 
         String code = normalizeCode(rawCode);
 
+        validateCurrencyCode(code);
+
         return new CurrencyRequest(name, code, sign);
+    }
+
+    // todo validator
+    private void validateCurrencyCode(String code) {
+        if (!code.matches("[A-Z]{3}")) {
+            throw new BadRequestException(INVALID_CODE_MESSAGE);
+        }
     }
 }
