fe489e51a312722faa99b7d741ee578bc6e9f1e8
aveasura <aveasura@gmail.com>
2026-04-01 10:28:17 +0300

    fix: add json annotation

--- src/main/java/org/exchanger/dto/response/UpdateExchangeRateResponse.java
+++ src/main/java/org/exchanger/dto/response/UpdateExchangeRateResponse.java
@@ -1,7 +1,10 @@
 package org.exchanger.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.math.BigDecimal;
 
+@JsonPropertyOrder({"id", "baseCurrency", "targetCurrency", "rate"})
 public record UpdateExchangeRateResponse (
         Long id,
         CurrencyResponse baseCurrency,
