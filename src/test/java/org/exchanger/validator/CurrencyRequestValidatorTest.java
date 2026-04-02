059a6bf163130fbf1b1b28c833951859a6325bc8
aveasura <aveasura@gmail.com>
2026-04-02 10:46:55 +0300

    test: add currency request validator

---
+++ src/test/java/org/exchanger/validator/CurrencyRequestValidatorTest.java
@@ -0,0 +1,46 @@
+package org.exchanger.validator;
+
+import org.exchanger.dto.request.CurrencyRequest;
+import org.exchanger.exception.BadRequestException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CurrencyRequestValidatorTest {
+
+    private final CurrencyRequestValidator validator = new CurrencyRequestValidator();
+
+    @Test
+    void shouldValidateCorrectRequest() {
+        CurrencyRequest request = new CurrencyRequest("Euro", "EUR", "€");
+
+        assertDoesNotThrow(() -> validator.validate(request));
+    }
+
+    @Test
+    void shouldThrowWhenNameIsBlank() {
+        CurrencyRequest request = new CurrencyRequest("     ", "EUR", "€");
+
+        BadRequestException exception = assertThrows(BadRequestException.class,
+                () -> validator.validate(request)
+        );
+
+        assertEquals("Field 'name' required", exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowWhenCodeIsInvalid() {
+        CurrencyRequest request = new CurrencyRequest("Euro", "EURO", "€");
+
+        BadRequestException exception = assertThrows(BadRequestException.class,
+                () -> validator.validateCode(request.code())
+        );
+
+        assertEquals(
+                "Currency code should contain exactly 3 latin letters. Example: EUR",
+                exception.getMessage()
+        );
+    }
+}
